334bd47ea8bfda0beff996e2a90550b3e5908af1
oguz-sahin <oguzsahin098@gmail.com>
2020-05-21 15:30:13 +0300

    card firebase

--- app/src/main/java/com/example/firstapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firstapplication.ui.activity
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -14,12 +15,10 @@ import com.example.firstapplication.model.MenuModel
 import com.example.firstapplication.namazvaktiApi.*
 import com.example.firstapplication.ui.fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONArray
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.IOException
-import java.time.LocalDateTime
+import java.time.LocalTime
 
 class MainActivity : AppCompatActivity() {
     private val ApiClientSehir: SehirApiService by lazy { SehirApiClient.getApiClient() }
@@ -35,7 +34,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        read_json()
+
         linearLayout.Gone()
         progressBarMain.Visible()
 
@@ -103,31 +102,70 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<VakitModel>, response: Response<VakitModel>) {
 
                 if (response.isSuccessful) {
-                    var array = ArrayList<String>()
-                    val vakit = response.body()!!.get(0).aksam
-                    for (i in vakit) {
-                        if (i.toString().equals(":")) continue
-                        array.add(i.toString())
+
+                    val time = LocalTime.now()
+
+                    val imsak = response.body()!!.get(0).ımsak
+                    val gunes = response.body()!!.get(0).gunes
+                    val ogle = response.body()!!.get(0).ogle
+                    val ikindi = response.body()!!.get(0).ıkindi
+                    val aksam = response.body()!!.get(0).aksam
+                    val yatsi = response.body()!!.get(0).yatsi
+
+                    val imsakf = LocalTime.parse(imsak)
+                    val gunesf = LocalTime.parse(gunes)
+                    val oglef = LocalTime.parse(ogle)
+                    val ikindif = LocalTime.parse(ikindi)
+                    val aksamf = LocalTime.parse(aksam)
+                    val yatsif = LocalTime.parse(yatsi)
+
+                    var kalanVakit: String
+
+
+                    if (yatsif.isBefore(time) && imsakf.isAfter(time)) {
+                        yatsi_tv.setTextColor(getColor(R.color.Sari))
+                        yatsi_values_tv.setTextColor(getColor(R.color.Sari))
+                        kalanVakit =
+                            (imsakf.hour - time.hour).toString() + (imsakf.minute - time.minute).toString()
+                    } else if (imsakf.isBefore(time) && gunesf.isAfter(time)) {
+                        imsak_tv.setTextColor(getColor(R.color.Sari))
+                        imsak_values_tv.setTextColor(getColor(R.color.Sari))
+                        kalanVakit =
+                            (gunesf.hour - time.hour).toString() + (gunesf.minute - time.minute).toString()
+                    } else if (gunesf.isBefore(time) && oglef.isAfter(time)) {
+                        günes_tv.setTextColor(getColor(R.color.Sari))
+                        günes_values_tv.setTextColor(getColor(R.color.Sari))
+                        kalanVakit =
+                            (oglef.hour - time.hour).toString() + ":" + (oglef.minute - time.minute).toString()
+
+
+                        Toast.makeText(this@MainActivity, kalanVakit, Toast.LENGTH_LONG).show()
+                    } else if (oglef.isBefore(time) && ikindif.isAfter(time)) {
+                        ogle_tv.setTextColor(getColor(R.color.Sari))
+                        ogle_values_tv.setTextColor(getColor(R.color.Sari))
+
+                    } else if (ikindif.isBefore(time) && aksamf.isAfter(time)) {
+                        ikindi_tv.setTextColor(getColor(R.color.Sari))
+                        ikindi_values_tv.setTextColor(getColor(R.color.Sari))
+
+                    } else if (aksamf.isBefore(time) && yatsif.isAfter(time)) {
+                        aksam_tv.setTextColor(getColor(R.color.Sari))
+                        aksam_values_tv.setTextColor(getColor(R.color.Sari))
+
                     }
-                    val current = LocalDateTime.now()
-                    val currentHour = current.hour
-                    val currentMinute = current.minute
-                    val currentSecond = current.second
 
 
-                    val aksamSaat = (array[0] + array[1]).toInt()
-                    val aksamDakika = (array[2] + array[3]).toInt()
 
-                    Log.e("aksam saat", aksamSaat.toString() + aksamDakika.toString())
 
-                    linearLayout.Visible()
+                    imsak_values_tv.text = imsak
+                    günes_values_tv.text = gunes
+                    ogle_values_tv.text = ogle
+                    ikindi_values_tv.text = ikindi
+                    aksam_values_tv.text = aksam
+                    yatsi_values_tv.text = yatsi
+
                     progressBarMain.Gone()
-                    imsak_values_tv.text = response.body()!!.get(0).ımsak
-                    günes_values_tv.text = response.body()!!.get(0).gunes
-                    ogle_values_tv.text = response.body()!!.get(0).ogle
-                    ikindi_values_tv.text = response.body()!!.get(0).ıkindi
-                    aksam_values_tv.text = response.body()!!.get(0).aksam
-                    yatsi_values_tv.text = response.body()!!.get(0).yatsi
+                    linearLayout.Visible()
 
 
                 }
@@ -141,29 +179,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun read_json() {
 
 
-        var json: String? = null
-        try {
-            val inputStream = assets.open("esmaulhuna.json")
-            json = inputStream.bufferedReader().use { it.readText() }
-            var jsonArray = JSONArray(json)
-
-            for (i in 0..jsonArray.length() - 1) {
-
-                var jsonobj = jsonArray.getJSONObject(i)
-
-                EsmaulHusnaArray.add(jsonobj.getString("isim"))
 
 
-            }
-
-        } catch (e: IOException) {
-
-
-        }
-        Log.e("esmaul hunsna", EsmaulHusnaArray.size.toString())
-
-    }
 }
